4d6ec2cdf15ce199d69c21a454203d548e431e08
Robbie Bart <adm@AdminisatorsAir.lan>
2020-06-16 20:15:59 -0400

    q1

--- workshop/exercise-1.js
+++ workshop/exercise-1.js
@@ -30,22 +30,51 @@ const notCode = [
 
 // Q1.1
 // How many names does Morty have? (use grandson)
-
+let mortyNamesArray = grandson.split(' ');
+let numberOfNames = mortyNamesArray.length;
+console.log(`A1) Morty has ${numberOfNames} names`);
 // Q1.2
 // Convert the alternateRicks string to an array. Console that array.
+let alternateRicksArray = alternateRicks.split(', ');
+console.log(alternateRicksArray);
 
 // Q1.3
 // How many Ricks have been named in alternateRicks?
+let numberOfRicks = 0;
+for (let rickCounter = 0; rickCounter < alternateRicksArray.length; rickCounter++ ) {
+  if (alternateRicksArray[rickCounter].includes('Rick')) {
+    numberOfRicks +=1;
+  }
+} console.log(`There are ${numberOfRicks} in alternateRicks`);
 
 // Q1.4
 // How many characters are there in rickSaying (without spaces)?
+let rickSayingCollapsed = rickSaying.split("");
+let counter = 0;
+for (let i = 0; i < rickSayingCollapsed.length; i++) {
+  if (rickSayingCollapsed[i] !== ' ') {
+      counter += 1;
+  }
+} console.log(`There are ${counter} characters in rickSaying`)
+//mostly found this with solution
 
 // Q1.4
 // Morty doesn't actually have a middle name. Console Morty's name without "Antoine".
+let mortyFirstName = grandson.slice(0, 5);
+let mortyLastName = grandson.slice(14);
+let mortyFullName = (mortyFirstName.concat(' ').concat(mortyLastName));
+console.log(mortyFullName);
 
 // Q1.5
 // Decode and console. the secret!
 // To do so, you will need to remove all of the notCode words.
+
+let decoded = notCode;
+for (let i = 0; i < notCode.length; i++ { 
+  decoded = decode.replace(notCode[i], ''));
+}
+console.log (decoded);
+//mostly found this with solution
 
 // Q1.6 (Stretch Goal)
 // Scary Terry is actually not an enemy. Remove him from the list and console only Rick's true enemies.
